2cb7b039df87888831e435852f8f25108005103a
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-12-08 02:38:12 +0200

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+floor which takes a float n as argument and returns the floor of the float
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    floor which takes a float n as argument
+
+    parameter:
+    n (float): first parameter
+
+    Returns:
+    int: integer
+    """
+    return math.floor(n)
