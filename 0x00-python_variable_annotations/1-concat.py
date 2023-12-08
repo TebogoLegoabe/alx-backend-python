2c697b74239d928c2bbee0c56cd8b0cb5364e8b7
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-12-08 02:15:56 +0200

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+concat that takes a string str1 and a string str2 as arguments 
+and returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    fuction that takes 2 strings combine them
+
+    parameters:
+    str1(string): first parameter
+    str2(string): secound parameter
+
+    Return:
+    combined string
+    """
+    return str1 + str2
