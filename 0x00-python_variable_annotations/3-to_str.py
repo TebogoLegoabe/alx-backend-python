16f6c9eff7b29867ea9a053d5e66e0ac3b096bf0
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-12-08 02:43:28 +0200

    Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Function that converts a float to a string
+"""
+def to_str(n: float) -> str:
+    """
+    function converts a float to a string and returns string
+
+    parameters:
+    n (float): first parameter
+
+    Returns:
+    str: converted float
+    """
+    return str(n)
