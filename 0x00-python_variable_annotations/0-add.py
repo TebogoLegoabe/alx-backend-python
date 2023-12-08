c5781d6d32a7736124185663b91b28f6bab1fc48
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-12-08 02:04:47 +0200

    Basic annotations - add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -5,6 +5,10 @@
 
 def add(a:float, b:float) -> float:
     """
+    function that adds two floats
+    parameters:
+    a(float): the first parameter
+    b(float): the secound parameter
     Returns:
     The sum of a and b
     """
