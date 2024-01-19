93ed78ae532d9161b984225b463e3351f95d35c9
TebogoLegoabe <tebogolegoab5@gmail.com>
2024-01-19 15:58:25 +0200

    parameterize a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -8,7 +8,6 @@ from unittest.mock import patch, Mock
 from parameterized import parameterized
 from utils import access_nested_map
 
-
 class TestAccessNestedMap(unittest.TestCase):
 
     @parameterized.expand([
@@ -18,6 +17,16 @@ class TestAccessNestedMap(unittest.TestCase):
     ])
     def test_access_nested_map(self, nested_map, path, expected_result):
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
+
+    @parameterized.expand([
+        ({}, ("a",), KeyError, "Key 'a' not found in the nested map"),
+        ({"a": 1}, ("a", "b"), KeyError, "Key 'b' not found in the nested map"),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, exception_type, expected_message):
+        with self.assertRaises(exception_type) as context:
+            access_nested_map(nested_map, path)
+
+        self.assertEqual(str(context.exception), expected_message)
 
 if __name__ == "__main__":
     unittest.main()
